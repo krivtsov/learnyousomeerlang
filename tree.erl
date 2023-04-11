7dbf9e834337f8325bb5bb9a411fa2f05fdc1f27
Krivtsov <krivtsov.su@gmail.com>
2023-04-11 23:55:11 +0400

    lint tree

--- tree.erl
+++ tree.erl
@@ -3,21 +3,20 @@
 
 empty() -> {node, 'nil'}.
 
-insert(Key, Val, {node, 'nil'}) -> {node, {Key, Val, {node, 'nil'}, {node, 'nil'}}};
-insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}}) when
-  NewKey < Key ->
-    {node, {Key, Val, insert(NewKey, NewVal, Smaller), Larger}}; 
-insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}}) when
-  NewKey > Key ->
-    {node, {Key, Val, Smaller, insert(NewKey, NewVal, Larger)}};
-insert(Key, Val, {node, {Key, _, Smaller, Larger}}) -> {node, {Key, Val, Smaller, Larger}}.
+insert(Key, Val, {node, 'nil'}) ->
+  {node, {Key, Val, {node, 'nil'}, {node, 'nil'}}};
+insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}}) when NewKey < Key ->
+  {node, {Key, Val, insert(NewKey, NewVal, Smaller), Larger}};
+insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}}) when NewKey > Key ->
+  {node, {Key, Val, Smaller, insert(NewKey, NewVal, Larger)}};
+insert(Key, Val, {node, {Key, _, Smaller, Larger}}) ->
+  {node, {Key, Val, Smaller, Larger}}.
 
-lookup(_, {node, 'nil'}) -> undefined;
-lookup(Key, {node, {Key, Val, _, _}}) -> {ok, Val};
-lookup(Key, {node, {NodeKey, _, Smaller, _}}) 
-  when Key < NodeKey 
-    -> lookup(Key, Smaller);
-lookup(Key, {node, {_, _, _, Larger}}) -> lookup(Key, Larger).
-
-
-
+lookup(_, {node, 'nil'}) ->
+  undefined;
+lookup(Key, {node, {Key, Val, _, _}}) ->
+  {ok, Val};
+lookup(Key, {node, {NodeKey, _, Smaller, _}}) when Key < NodeKey ->
+  lookup(Key, Smaller);
+lookup(Key, {node, {_, _, _, Larger}}) ->
+  lookup(Key, Larger).
